95500911b64373ca8f33155b5f7364319aa90d84
Kevin Quiñones <82183566+KevinQ73@users.noreply.github.com>
2024-04-25 21:54:46 -0300

    Update script.js

--- keygen/script.js
+++ keygen/script.js
@@ -1,15 +1,28 @@
+var i = 0;
+
 function multiplicar() {
   var numero = document.getElementById("numeroInput").value;
   var resultado = document.getElementById("resultado");
+  var imagen = document.getElementById("imagen");
 
   // Verificar si se ingresó un número válido
   if (isNaN(numero) || numero.length > 4) {
-    resultado.textContent = "Debe ingresar un valor que no supere los 4 dígitos.";
+    resultado.textContent = "Por favor, ingrese un número válido de máximo 4 dígitos.";
+    i++;
+    // Si el último número ingresado también es inválido, mostrar la imagen
+    if (i >= 2) {
+      imagen.style.display = "block";
+      resultado.textContent = "Te dije, un número de máximo 4 dígitos.";
+    }
   } else {
     // Realizar la operación y redondear hacia abajo
     var resultadoOperacion = Math.floor(((numero * 13362) / 12) + 1991);
-    resultado.textContent = "La key resultante es: " + resultadoOperacion;
+    resultado.textContent = "La Key generada es: " + resultadoOperacion;
+    imagen.style.display = "none"; // Ocultar la imagen si se ingresa un número válido
+    i = 0;
   }
+
+  ultimoNumero = numero; // Guardar el último número ingresado
 }
 
 // Listener de evento para la tecla "Enter"
